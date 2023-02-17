ea0a51616c83e48f188705e1f089ced4e7676794
Cote Phinnizee <ucphinni@gmail.com>
2023-02-17 17:45:28 -0500

    test

--- src/setup.pl
+++ src/setup.pl
@@ -110,9 +110,9 @@ sub setup_iptables(){
     system qw(iptables -t mangle -A SSREDIR -m owner --uid-owner root -j RETURN ) or die $!;
     system qw(iptables -t mangle -A SSREDIR -j CONNMARK --restore-mark) or die $!;
     system qw(iptables -t mangle -A SSREDIR -m mark --mark 0x2333 -j RETURN) or die $!;
-    for my $ip qw(0.0.0.0/8 10.0.0.0/8 100.64.0.0/10 127.0.0/8 169.254.0.0/16 172.16.0.0/12 192.0.0.0/24 \
-		  192.0.2.0/24 192.88.99.0/24 192.168.0.0/16 198.18.0.0/15 198.51.100.0/24 203.0.113.0/24 \
-	         224.0.0.0/4 240.0.0.0/4 255.255.255.255/32) {
+    for my $ip (qw(0.0.0.0/8 10.0.0.0/8 100.64.0.0/10 127.0.0/8 169.254.0.0/16 172.16.0.0/12 192.0.0.0/24
+		  192.0.2.0/24 192.88.99.0/24 192.168.0.0/16 198.18.0.0/15 198.51.100.0/24 203.0.113.0/24
+	         224.0.0.0/4 240.0.0.0/4 255.255.255.255/32)) {
 	system qw"iptables -t mangle -A SSREDIR -d $ip" or die $!;
 
     }
