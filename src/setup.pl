b384fa4d256b2b40f673576248802ec528cd3f99
Cote Phinnizee <ucphinni@gmail.com>
2023-02-17 18:08:07 -0500

    test

--- src/setup.pl
+++ src/setup.pl
@@ -116,7 +116,7 @@ sub setup_iptables(){
     for my $ip (qw(0.0.0.0/8 10.0.0.0/8 100.64.0.0/10 127.0.0/8 169.254.0.0/16 172.16.0.0/12 192.0.0.0/24
 		  192.0.2.0/24 192.88.99.0/24 192.168.0.0/16 198.18.0.0/15 198.51.100.0/24 203.0.113.0/24
 	         224.0.0.0/4 240.0.0.0/4 255.255.255.255/32)) {
-	system qw"iptables -t mangle -A SSREDIR -d $ip" and die $!;
+	system qw(iptables -t mangle -A SSREDIR -d), $ip and die $!;
 
     }
     system qw"iptables -t mangle -A SSREDIR -p tcp --syn -j MARK --set-mark 0x2333" and die $!;
