890b2712d52d1169ef93a1ffde7339496023b341
Cote Phinnizee <ucphinni@gmail.com>
2023-02-16 23:08:19 -0500

    test

--- src/setup.pl
+++ src/setup.pl
@@ -34,7 +34,7 @@ sub set_repo_file_and_upgrade($$) {
 sub check_repo_version_valid() {
     my ($url,$ver) = get_repo_url_line();
     my $urlstr = "$url/$ver/main";
-    `wget -qO '$urlstr'  > /dev/null`;
+    qx"wget -qO '$urlstr'  > /dev/null";
     $? != 0 and return undef;
     return 1;
 }
