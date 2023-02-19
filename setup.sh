eaad168c57fc37459c967260f99c0fa821b44446
Cote Phinnizee <ucphinni@gmail.com>
2023-02-19 16:11:01 -0500

    test

--- setup.sh
+++ setup.sh
@@ -10,8 +10,8 @@ which git > /dev/null
 let w_git=$?
 
 mount -o remount,size=128K    /run
-mount -o remount,size=350000K /
-apk add miniperl git alpine-sdk build-base apk-tools alpine-conf \
+mount -o remount,size=360000K /
+apk add git alpine-sdk build-base apk-tools alpine-conf \
     busybox fakeroot syslinux xorriso squashfs-tools
 abuild-keygen -i -a
 git clone https://github.com/ucphinni/ssmd.git
